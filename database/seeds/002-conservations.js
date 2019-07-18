5bbfa03f6ef623b9f70458f19da383cd541e571d
Ba-Zen <bazenatlas@gmail.com>
2019-07-18 15:40:34 -0400

    deleted user seeds

---
+++ database/seeds/002-conservations.js
@@ -0,0 +1,37 @@
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('conservations')
+    .del()
+    .then(function() {
+      // Inserts seed entries
+      return knex('conservations').insert([
+        {
+          id: 1,
+          org_name: 'Beaver Town',
+          org_link: 'google.com',
+          about_us: 'Lorem IpsOn',
+          species: 'beavers',
+          habitats: 'da dam',
+          issues: 'wood'
+        },
+        {
+          id: 2,
+          org_name: 'Panda Lovers',
+          org_link: 'google.com',
+          about_us: 'Lorem IpsOn',
+          species: 'pandas',
+          habitats: 'forest',
+          issues: 'sleep'
+        },
+        {
+          id: 3,
+          org_name: 'Giraffe 2 Tall',
+          org_link: 'google.com',
+          about_us: 'Lorem IpsOn',
+          species: 'giraffes',
+          habitats: 'savannah',
+          issues: 'too tall'
+        }
+      ]);
+    });
+};
