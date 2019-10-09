621c7c89568829cac1a62855ad31bd6d8bae84b1
Samantha Egge <samantha.c.egge@gmail.com>
2019-10-09 12:59:30 -0700

    Created species_habitats and many-many spec to conservations migration.

---
+++ database/migrations/20191009123517_species_habitats.js
@@ -0,0 +1,45 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema
+    .createTable('speciesHabitats', species => {
+      species.increments('spec_hab_id');
+      species.text('spec_hab_name')
+        .unique()
+        .notNullable()
+      species.string('spec_hab_photo')
+        .notNullable()
+      species.text('spec_hab_type')
+        .notNullable()
+        // either 'species' or 'habitat'
+    })
+    .createTable('conservationCats', cats => {
+      cats.increments('consv_cat_id');
+      cats.integer('conservation_id')
+        .notNullable()
+        .unsigned()
+        .references('cons_id')
+        .inTable('conservationists')
+      cats.integer('spec_hab_id')
+        .notNullable()
+        .unsigned()
+        .references('spec_hab_id')
+        .inTable('speciesHabitats')
+    })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema
+    .dropTableIfExists('campaignCats')
+    .dropTableIfExists('speciesHabitats')
+};
+
+
+// search turtle
+// - campaigns
+// - conservationists
+// - species
+// if ('species.type' === 'species') {
+//   do this
+// } else {
+//   do that
+// }
