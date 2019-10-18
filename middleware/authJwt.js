a3e3a6487c78a25b9b6847ac0a1898fd5cf4939e
Samantha Egge <samantha.c.egge@gmail.com>
2019-10-17 17:08:13 -0700

    Fixed spelling of import.

---
+++ middleware/authJwt.js
@@ -0,0 +1,28 @@
+const jwt = require('express-jwt');
+const jwtAuthz = require('express-jwt-authz');
+const jwksRsa = require('jwks-rsa');
+
+// # Auth0 Middleware # //
+const authConfig = {
+  domain: 'key-conservation.auth0.com',
+  audience: 'https://key-conservation'
+};
+
+// Define middleware that validates incoming bearer tokens
+// using JWKS from YOUR_DOMAIN
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`
+  }),
+
+  audience: authConfig.audience,
+  issuer: `https://${authConfig.domain}/`,
+  algorithm: ['RS256']
+});
+console.log(checkJwt);
+
+module.exports = checkJwt
+// # End Auth0 Middleware # //
