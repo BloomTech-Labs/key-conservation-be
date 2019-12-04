63ff048192ddb98bce7daaf1a91a33570c7a9fe8
bodhibrdr <julie.gumerman@gmail.com>
2019-12-03 18:02:23 -0700

    basic authorization tests of all routes complete

--- __tests__/campaigns.test.js
+++ __tests__/campaigns.test.js
@@ -1,10 +1,18 @@
 const supertest = require("supertest");
 const server = require("../api/server.js");
+const db = require('../database/dbConfig')
 
-describe("social routes", () => {
+describe("user routes", () => {
+    beforeEach(async () => {
+        await db("campaigns").truncate();
+    })
+})
+
+
+describe("campaign routes", () => {
     it (" will not let you access social route without a token", async () => {
            await supertest(server)
-                .get("/api/social")
+                .get("/api/campaigns")
                 .expect(401) 
     })
 })
