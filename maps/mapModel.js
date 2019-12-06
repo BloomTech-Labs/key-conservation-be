547b013cd946cb77e2abf5708be80e433aeedad8
bodhibrdr <julie.gumerman@gmail.com>
2019-12-06 11:59:59 -0700

    authorization

--- maps/mapModel.js
+++ maps/mapModel.js
@@ -2,7 +2,7 @@ const db = require('../database/dbConfig.js');
 
 const findConservationOrganizations = () => {
     return db("users")
-        .join("conservationists as cons", "users.id", "cons.cons_id")
+        .join("conservationists as cons", "users.id", "cons.users_id")
         .where("roles", "conservationist")
         // .include()
         // .where("")
