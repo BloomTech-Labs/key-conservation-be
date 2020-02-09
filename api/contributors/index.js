857d5438d4e1d7d721be07c386704b129695ba88
David Cruz <david@typokign.com>
2020-02-09 15:52:24 -0800

    Add GET /api/contributors
    Added GET /api/contributors to find local supporters with certain skill
    sets to contribute to campaigns.
    
    Accepts query parameters:
        - skills (string[]): Array of skills to search for
        - distance (number): Distance in miles to search
        - lat (number): Searcher latitude, required for distance
        - long (number): Searcher longitude, required for distance
    
    Returns an array of matching users.

---
+++ api/contributors/index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+
+const db = require('../../database/dbConfig');
+const restricted = require('../../middleware/authJwt');
+const Skills = require('../../models/skills');
+
+const router = express.Router();
+
+router.get('/', /*restricted,*/ async (req, res) => {
+  const { distance, lat, long } = req.query;
+  const skills = req.query.skills ?
+    req.query.skills
+      .split(',')
+      .map(skill => skill.toUpperCase())
+    : [];
+
+  if (skills) {
+    const unknownSkills = [];
+    for (const skill of skills) {
+      if (!Skills[skill]) {
+        unknownSkills.push(skill);
+      }
+    }
+
+    if (unknownSkills.length > 0) {
+      return res.status(400).json({ err: `unknown skills: ${unknownSkills.join(',')}`});
+    }
+  }
+
+  if (distance && isNaN(distance)) {
+    return res.status(400).json({ err: 'distance must be a number' });
+  }
+
+  if (distance && distance < 0) {
+    return res.status(400).json({ err: 'distance must be positive' });
+  }
+
+  if (distance && !(lat && long)) {
+    return res.status(400).json({ err: 'must specify current location in lat and long fields to use distance' });
+  }
+
+  const query = db.raw(`
+  SELECT *, array_to_json(skills) as skills FROM (
+    SELECT users.*, array_agg(skills.skill) AS skills
+    FROM users
+    INNER JOIN skills ON skills.user_id = users.id
+    GROUP BY users.id
+  ) AS joined
+  WHERE skills @> :skills
+  `, { skills });
+
+  const users = (await query).rows;
+
+  res.status(200).send(users);
+});
+
+module.exports = router;
