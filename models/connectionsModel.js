c67d262a8c47cb5cbe8ebec107548299c3eb89c0
Rasha Moumneh <rasha@fastmail.com>
2020-02-13 13:57:39 -0500

    knex syntax fix

--- models/connectionsModel.js
+++ models/connectionsModel.js
@@ -24,11 +24,7 @@ function getConnectionById(id) {
 // use this for adding a connection or sending a connection request
 function addConnection(connectorId, connectedId, status) {
   return db('connections')
-    .insert([
-      { connector_id: connectorId },
-      { connected_id: connectedId },
-      { status }
-    ])
+    .insert([{ connector_id: connectorId, connected_id: connectedId, status }])
     .returning('connection_id');
 }
 
