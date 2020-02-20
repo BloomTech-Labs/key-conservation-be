86db601d5e056a4addac2caf25de44a7fcd52fb7
shravanhariharan2 <smhariha@ucsd.edu>
2020-02-19 17:16:27 -0800

    edit shorthand var name and remove unnecessary API route

--- api/submissions/index.js
+++ api/submissions/index.js
@@ -2,28 +2,14 @@ const express = require("express");
 
 const router = express.Router();
 
-const Sub = require("../../models/submissionsModel");
-
-// this async is needed, "pg-native" module not found error if not async, but why????
-router.get("/", async (req, res) => {
-  try {
-    const subs = await Sub.find();
-    if (subs) {
-      res.status(200).json({ subs });
-    } else {
-      res.status(400).json({ message: "Submissions not found in the database" });
-    }
-  } catch (err) {
-    res.status(500).json({ err, msg: "Unable to make request to server" });
-  }
-});
+const Submission = require("../../models/submissionsModel");
 
 router.get("/:id", async (req, res) => {
   try {
     const { id } = req.params;
-    const sub = await Sub.findById(id);
-    if (sub) {
-      res.status(200).json({ sub });
+    const submission = await Submission.findById(id);
+    if (submission) {
+      res.status(200).json({ submission });
     } else {
       res.status(400).json({ message: "Submission not found in the database" });
     }
@@ -38,9 +24,9 @@ router.post("/", async (req, res) => {
     decision: "PENDING"
   };
   try {
-    const newSub = await Sub.insert(postSub);
-    if (newSub) {
-      res.status(201).json({ newSub, msg: "Submission added to database" });
+    const newSubmission = await Submission.insert(postSub);
+    if (newSubmission) {
+      res.status(201).json({ newSubmission, msg: "Submission added to database" });
     }
   } catch (err) {
     res.status(500).json({ err, msg: "Unable to add submission" });
@@ -52,11 +38,11 @@ router.put("/:id", async (req, res) => {
   const { decision } = req.body;
 
   try {
-    const updatedSub = await Sub.update(decision, id);
-    if (updatedSub) {
+    const updatedSubmission = await Submission.update(decision, id);
+    if (updatedSubmission) {
       // still need to change all other submissions to denied under the same request,
       // but need skilled impact model for that. for now, just update that submission
-      res.status(201).json({ updatedSub, msg: "Submission updated in database" });
+      res.status(201).json({ updatedSubmission, msg: "Submission updated in database" });
     }
   } catch (err) {
     res.status(500).json({ err, msg: "Unable to update submission" });
--- models/submissionsModel.js
+++ models/submissionsModel.js
@@ -1,41 +1,33 @@
 const db = require("../database/dbConfig");
 
-// not sure if I need async here or not either
-function find() {
-  return db("application_submissions");
-}
-
 function findById(id) {
   return db("application_submissions")
     .where({ id })
     .first();
 }
 
-// same here, not sure if needed
 async function insert(submission) {
-  // .returning("id") returns array with id, so need [id] to capture value
   const [id] = await db("application_submissions")
     .insert(submission)
     .returning("id");
   if (id) {
-    const sub = await findById(id);
-    return sub;
+    const submission = await findById(id);
+    return submission;
   }
 }
 
 async function update(decision, id) {
-  const [sub_id] = await db("application_submissions")
+  const [submission_id] = await db("application_submissions")
     .where({ id })
     .update({decision})
     .returning("id");
-  if (sub_id) {
-    const sub = await findById(sub_id);
-    return sub;
+  if (submission_id) {
+    const submission = await findById(submission_id);
+    return submission;
   }
 }
 
 module.exports = {
-  find,
   findById,
   insert,
   update
