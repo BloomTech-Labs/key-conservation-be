792cc00c3a9aa37fe3b8b0178fa6ff7e7abf789b
ThomasLiARDJAVA <thomasligreat11@gmail.com>
2020-02-24 18:46:40 -0800

    added skillimpactrequest model and campaign api change

--- models/campaignModel.js
+++ models/campaignModel.js
@@ -4,93 +4,94 @@ const db = require('../database/dbConfig');
 const CampUpdate = require('./updateModel.js');
 const CampComments = require('./commentsModel.js');
 const CampLikes = require('./socialModel.js');
-
+const skills = require('../skilledImpactRequestsModel.js');
 
 function find() {
   return db('campaigns')
-    .join('users', 'users.id', 'campaigns.users_id')
-    .select(
-      'users.username',
-      'users.profile_image',
-      'users.location',
-      'campaigns.*'
-    )
-    .then(campaigns =>
-      db('likes').then(likes => {
-        campaigns.map(
-          cam =>
-            (cam.likes = likes.filter(like => like.camp_id === cam.camp_id))
-        );
-        return campaigns;
-      })
-    )
-    .then(campaigns =>
-      db('comments')
-        .join('users', 'users.id', 'comments.users_id')
-        .select('comments.*', 'users.profile_image', 'users.username', 'users.is_deactivated')
-        .then(comments => {
-          campaigns.map(
-            cam =>
-              (cam.comments = comments.filter(
-                com => {
-                  return com.camp_id === cam.camp_id && !com.is_deactivated;
-                }))
-          );
-          return campaigns;
-        })
-    )
-    .then(campaigns =>
-      db('users').then(users => {
-        return campaigns.filter(camp => {
-          const [user] = users.filter(user => user.id === camp.users_id);
-          if (user.is_deactivated) {
-            return false;
-          } else return true;
-        });
-      })
-    );
+      .join('users', 'users.id', 'campaigns.users_id')
+      .select(
+          'users.username',
+          'users.profile_image',
+          'users.location',
+          'campaigns.*'
+      )
+      .then(campaigns =>
+          db('likes').then(likes => {
+            campaigns.map(
+                cam =>
+                    (cam.likes = likes.filter(like => like.camp_id === cam.camp_id))
+            );
+            return campaigns;
+          })
+      )
+      .then(campaigns =>
+          db('comments')
+              .join('users', 'users.id', 'comments.users_id')
+              .select('comments.*', 'users.profile_image', 'users.username', 'users.is_deactivated')
+              .then(comments => {
+                campaigns.map(
+                    cam =>
+                        (cam.comments = comments.filter(
+                            com => {
+                              return com.camp_id === cam.camp_id && !com.is_deactivated;
+                            }))
+                );
+                return campaigns;
+              })
+      )
+      .then(campaigns =>
+          db('users').then(users => {
+            return campaigns.filter(camp => {
+              const [user] = users.filter(user => user.id === camp.users_id);
+              if (user.is_deactivated) {
+                return false;
+              } else return true;
+            });
+          })
+      );
 }
 
 function findCampaign(camp_id) {
   return db('campaigns')
-    .where({ camp_id })
-    .first();
+      .where({ camp_id })
+      .first();
 }
 
 async function findById(camp_id) {
   const campaign = await db('campaigns')
-    .where({ camp_id })
-    .join('users', 'users.id', 'campaigns.users_id')
-    .select(
-      'users.username',
-      'users.profile_image',
-      'users.location',
-      'users.is_deactivated',
-      'campaigns.*'
-    )
-    .first();
+      .where({ camp_id })
+      .join('users', 'users.id', 'campaigns.users_id')
+      .select(
+          'users.username',
+          'users.profile_image',
+          'users.location',
+          'users.is_deactivated',
+          'campaigns.*'
+      )
+      .first();
   campaign.updates = await CampUpdate.findUpdatesByCamp(camp_id);
   campaign.comments = await CampComments.findCampaignComments(camp_id);
   campaign.likes = await CampLikes.findCampaignLikes(camp_id);
+  campaign.skill_impact_requests = await skills.findSkills(camp_id);
   return campaign;
 }
 
 function findUser(id) {
   return db('users')
-    .where({ id })
-    .first();
+      .where({ id })
+      .first();
 }
 
 async function findCampByUserId(users_id) {
   const campaigns = await db('campaigns')
-    .where({ users_id })
-    .join('users', 'users.id', 'campaigns.users_id')
-    .select(
-      'users.username',
-      'users.profile_image',
-      'users.location',
-      'campaigns.*'
-    );
+      .where({ users_id })
+      .join('users', 'users.id', 'campaigns.users_id')
+      .select(
+          'users.username',
+          'users.profile_image',
+          'users.location',
+          'campaigns.*'
+      );
   const withUpdates = campaigns.map(async camp => {
     camp.updates = await CampUpdate.findUpdatesByCamp(camp.camp_id);
     camp.comments = await CampComments.findCampaignComments(camp.camp_id);
@@ -103,8 +104,8 @@ async function findCampByUserId(users_id) {
 
 async function insert(campaign) {
   const [camp_id] = await db('campaigns')
-    .insert(campaign)
-    .returning('camp_id');
+      .insert(campaign)
+      .returning('camp_id');
   if (camp_id) {
     const camp = await findById(camp_id);
     return camp;
@@ -113,8 +114,8 @@ async function insert(campaign) {
 
 async function update(campaign, camp_id) {
   const editedCamp = await db('campaigns')
-    .where({ camp_id })
-    .update(campaign);
+      .where({ camp_id })
+      .update(campaign);
   if (editedCamp) {
     const camp = await findById(camp_id);
     return camp;
@@ -123,8 +124,8 @@ async function update(campaign, camp_id) {
 
 async function remove(camp_id) {
   const deleted = await db('campaigns')
-    .where({ camp_id })
-    .del();
+      .where({ camp_id })
+      .del();
   if (deleted) {
     return camp_id;
   }
