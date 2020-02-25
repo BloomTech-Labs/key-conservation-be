e919ff5a892d450ed1c3d234719f59cc201272f2
ThomasLiARDJAVA <thomasligreat11@gmail.com>
2020-02-25 13:43:12 -0800

    reformatted the returned JSON(deleted redundant data)

--- models/campaignModel.js
+++ models/campaignModel.js
@@ -4,7 +4,7 @@ const db = require('../database/dbConfig');
 const CampUpdate = require('./updateModel.js');
 const CampComments = require('./commentsModel.js');
 const CampLikes = require('./socialModel.js');
-
+const skills = require('./skilledImpactRequestsModel');
 
 function find() {
   return db('campaigns')
@@ -72,6 +72,7 @@ async function findById(camp_id) {
   campaign.updates = await CampUpdate.findUpdatesByCamp(camp_id);
   campaign.comments = await CampComments.findCampaignComments(camp_id);
   campaign.likes = await CampLikes.findCampaignLikes(camp_id);
+  campaign.skill_impact_requests = await skills.findSkills(camp_id);
   return campaign;
 }
 
--- models/skilledImpactRequestsModel.js
+++ models/skilledImpactRequestsModel.js
@@ -1,19 +1,54 @@
 const db = require('../database/dbConfig');
 
 async function findSkills(campaign_id) {
-
-    return db('skilled_impact_requests')
+    return db('project_goals')
         .where({campaign_id})
+        .join('skilled_impact_requests', 'skilled_impact_requests.id','project_goals.skilled_impact_request_id')
         .select(
-            '*'
-        ).then((skilledImpactRequests)=>db('project_goals').then((projectGoals)=>{
-            skilledImpactRequests.map((sIR)=>(sIR.project_goals = projectGoals.filter(
-                (projectGoal) => projectGoal.skilled_impact_request_id === sIR.id,
-            )));
-
-            return skilledImpactRequests;
-        }));
-
+           '*'
+        )
+        .then((returnedQuery)=>{
+            let skilledRequestAndProjectGoal=[];
+            let projectGoalJSON = {
+                goal_title: returnedQuery[0].goal_title,
+                description: returnedQuery[0].description,
+            };
+            let skillAndProjectGoalJSON={
+                skill: returnedQuery[0].skill,
+                point_of_contact: returnedQuery[0].point_of_contact,
+                welcome_message: returnedQuery[0].welcome_message,
+                our_contribution: returnedQuery[0].our_contribution,
+                project_goals: [projectGoalJSON]
+            };
+            let skillRequestIndex = 0;
+            let skillID = returnedQuery[0].id;
+            skilledRequestAndProjectGoal.push(skillAndProjectGoalJSON);
+            for(let i = 1; i<returnedQuery.length; i++){
+                if(skillID == returnedQuery[i].id){
+                    projectGoalJSON = {
+                        goal_title: returnedQuery[i].goal_title,
+                        description: returnedQuery[i].description,
+                    };
+                    skilledRequestAndProjectGoal[skillRequestIndex].project_goals.push(projectGoalJSON);
+                }else{
+                    skillID = returnedQuery[i].id;
+                    skillRequestIndex++;
+                    projectGoalJSON = {
+                        goal_title: returnedQuery[i].goal_title,
+                        description: returnedQuery[i].description,
+                    };
+                    skillAndProjectGoalJSON={
+                        skill: returnedQuery[i].skill,
+                        point_of_contact: returnedQuery[i].point_of_contact,
+                        welcome_message: returnedQuery[i].welcome_message,
+                        our_contribution: returnedQuery[i].our_contribution,
+                        project_goals: [projectGoalJSON]
+                    };
+                    skilledRequestAndProjectGoal.push(skillAndProjectGoalJSON);
+                }
+            }
+            return skilledRequestAndProjectGoal;
+        });
 }
 
 module.exports = {
