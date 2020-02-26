8aae02b9e1b8cf1cc06c7810263178a647f305fd
ThomasLiARDJAVA <thomasligreat11@gmail.com>
2020-02-25 19:54:10 -0800

    let the loop handle adding the rows into map

--- models/skilledImpactRequestsModel.js
+++ models/skilledImpactRequestsModel.js
@@ -9,18 +9,7 @@ async function findSkills(campaign_id) {
         )
         .then((returnedQuery)=>{
             const skilledRequestAndProjectGoal = new Map();
-            let skillAndProject = {
-                skill: returnedQuery[0].skill,
-                point_of_contact: returnedQuery[0].point_of_contact,
-                welcome_message: returnedQuery[0].welcome_message,
-                our_contribution: returnedQuery[0].our_contribution,
-                project_goals:[{
-                goal_title: returnedQuery[0].goal_title,
-                description: returnedQuery[0].description,
-                }]
-            };
-            skilledRequestAndProjectGoal.set(returnedQuery[0].id, skillAndProject);
-            for(let i = 1; i<returnedQuery.length; i++){
+            for(let i = 0; i<returnedQuery.length; i++){
                 if(skilledRequestAndProjectGoal.has(returnedQuery[i].id)){
                     let projectGoal = {
                         goal_title: returnedQuery[i].goal_title,
@@ -29,7 +18,7 @@ async function findSkills(campaign_id) {
                     skilledRequestAndProjectGoal.get(returnedQuery[i].id).project_goals.push(projectGoal);
                 }
                 else{
-                    skillAndProject = {
+                    let skillAndProject = {
                         skill: returnedQuery[i].skill,
                         point_of_contact: returnedQuery[i].point_of_contact,
                         welcome_message: returnedQuery[i].welcome_message,
