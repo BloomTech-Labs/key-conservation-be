b628e5ad3820621a4963a9094a90fbd2d8fca243
ThomasLiARDJAVA <thomasligreat11@gmail.com>
2020-02-25 20:07:42 -0800

    changed conditional statement

--- models/skilledImpactRequestsModel.js
+++ models/skilledImpactRequestsModel.js
@@ -10,26 +10,25 @@ async function findSkills(campaign_id) {
         .then((returnedQuery)=>{
             const skilledRequestAndProjectGoal = new Map();
             for(let i = 0; i<returnedQuery.length; i++){
-                if(skilledRequestAndProjectGoal.has(returnedQuery[i].id)){
-                    let projectGoal = {
-                        goal_title: returnedQuery[i].goal_title,
-                        description: returnedQuery[i].description,
-                    };
-                    skilledRequestAndProjectGoal.get(returnedQuery[i].id).project_goals.push(projectGoal);
-                }
-                else{
+                if(!skilledRequestAndProjectGoal.has(returnedQuery[i].id)){
                     let skillAndProject = {
                         skill: returnedQuery[i].skill,
                         point_of_contact: returnedQuery[i].point_of_contact,
                         welcome_message: returnedQuery[i].welcome_message,
                         our_contribution: returnedQuery[i].our_contribution,
-                        project_goals:[{
-                            goal_title: returnedQuery[i].goal_title,
-                            description: returnedQuery[i].description,
-                        }]
+                        project_goals:[]
+                    };
+                    let projectGoal = {
+                        goal_title: returnedQuery[i].goal_title,
+                        description: returnedQuery[i].description,
                     };
                     skilledRequestAndProjectGoal.set(returnedQuery[i].id, skillAndProject);
                 }
+                let projectGoal = {
+                    goal_title: returnedQuery[i].goal_title,
+                    description: returnedQuery[i].description,
+                };
+                skilledRequestAndProjectGoal.get(returnedQuery[i].id).project_goals.push(projectGoal);
             }
             return Array.from(skilledRequestAndProjectGoal.values());
         });
