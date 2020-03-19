96cb1cdff387390ede4f80fcf2523e84fd335f0f
shravanhariharan2 <smhariha@ucsd.edu>
2020-03-19 10:22:54 -0700

    fix merge conflicts pt. 2

--- models/applicationSubmissionsModel.js
+++ models/applicationSubmissionsModel.js
@@ -2,15 +2,11 @@ const db = require('../database/dbConfig');
 
 async function findById(id) {
   return db('application_submissions')
-<<<<<<< HEAD
     .select('*')
-=======
->>>>>>> 1886a1b629b300e7828078c9e9daf53f5bc6835b
     .where({ id })
     .first();
 }
 
-<<<<<<< HEAD
 async function findAllByIds(ids) {
   return db('application_submissions')
     .select('*')
@@ -19,11 +15,6 @@ async function findAllByIds(ids) {
 
 async function findAllByCampaignId(campaign_id) {
   return db('skilled_impact_requests')
-=======
-async function findAllByCampaignId(campaign_id) {
-  return db('skilled_impact_requests')
-    .where({ campaign_id })
->>>>>>> 1886a1b629b300e7828078c9e9daf53f5bc6835b
     .join(
       'application_submissions',
       'skilled_impact_requests.id',
@@ -32,12 +23,8 @@ async function findAllByCampaignId(campaign_id) {
     .select(
       'skilled_impact_requests.campaign_id',
       'application_submissions.*'
-<<<<<<< HEAD
     )
     .where({ campaign_id });
-=======
-    );
->>>>>>> 1886a1b629b300e7828078c9e9daf53f5bc6835b
 }
 
 async function insert(submission) {
@@ -48,7 +35,6 @@ async function insert(submission) {
 
 async function update(id, decision) {
   return db('application_submissions')
-<<<<<<< HEAD
     .update({decision})
     .where({ id })
     .returning('*');
@@ -59,36 +45,10 @@ async function updateAll(ids, decision) {
     .update({ decision })
     .whereIn('id', ids)
     .returning('*');
-=======
-    .where({ id })
-    .update({decision})
-    .returning('*');
-}
-
-async function acceptAndDenyAllOthers(id, skilled_impact_request_id) {
-  await db.transaction(transaction => {
-    db('application_submissions')
-      .where({ id })
-      .update({ decision : 'ACCEPTED' })
-      .transacting(transaction)
-      .then(async () => {
-        await db('application_submissions')
-          .where({ skilled_impact_request_id })
-          .where('id', '<>', id)
-          .update({ decision: 'DENIED' })
-          .transacting(transaction);
-      })
-      .then(transaction.commit)
-      .catch(transaction.rollback);
-  })
-  
-  return findById(id);
->>>>>>> 1886a1b629b300e7828078c9e9daf53f5bc6835b
 }
 
 module.exports = {
   findById,
-<<<<<<< HEAD
   findAllByIds,
   findAllByCampaignId,
   insert,
