11ba7c62d763a3a5245a73f3ffca269dd8ad5a35
David Cruz <david@typokign.com>
2020-04-12 00:12:10 -0700

    Re-add Sensors, Camera Traps to available skills
    We deleted these during the refactor due to database weirdness. Looks
    like we forgot to delete them from the actual app enum
    (models/skillsEnum.js) lol, so no changes there in this PR.

---
+++ app/database/migrations/20200412001018_add_skills_sensors_cameras.js
@@ -0,0 +1,11 @@
+
+exports.up = function(knex) {
+  return knex.schema.raw(`
+    ALTER TYPE enum_skills ADD VALUE 'SENSORS' AFTER 'RESEARCH';
+    ALTER TYPE enum_skills ADD VALUE 'CAMERA_TRAPS' AFTER 'BUSINESS_DEVELOPMENT';
+  `);
+};
+
+exports.down = function(knex) {};
+
+exports.config = { transaction: false };
