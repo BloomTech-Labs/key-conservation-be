00cfc72ad1d2f6fc7f6518c2347a2ecc1a971f58
Sumeet Bansal <sumeetbansal@gmail.com>
2020-04-24 13:49:42 -0700

    comments model

--- app/database/models/commentsModel.js
+++ app/database/models/commentsModel.js
@@ -1,7 +1,5 @@
 const db = require('../dbConfig');
 
-// const CampUpdate = require('./updateModel');
-
 function find() {
   return db('comments');
 }
@@ -13,7 +11,7 @@ function findById(id) {
 }
 
 async function findCampaignComments(id) {
-  return db('comments')
+  const comments = await db('comments')
     .join('users', 'users.id', 'comments.user_id')
     .leftJoin('conservationists as cons', 'cons.user_id', 'users.id')
     .leftJoin('supporters as sup', 'sup.user_id', 'users.id')
@@ -25,60 +23,28 @@ async function findCampaignComments(id) {
       'cons.name as org_name',
       'sup.name as sup_name',
       'users.is_deactivated',
-    )
-    .then((res) => res.map((com) => ({ ...com, name: com.org_name || com.sup_name || 'User' })));
-}
-
-function insert(comment) {
-  return db('comments')
-    .insert(comment)
-    .then(
-      () => findCampaignComments(comment.campaign_id),
-      // TODO is this safe to delete?
-      // return db('campaigns')
-      //   .where({ camp_id: comment.camp_id })
-      //   .join('users', 'users.id', 'campaigns.user_id')
-      //   .select(
-      //     'users.username',
-      //     'users.profile_image',
-      //     'users.location',
-      //     'campaigns.*'
-      //   )
-      //   .first()
-      //   .then(campaign => {
-      //     return CampUpdate.findUpdatesByCamp(comment.camp_id)
-      //       .then(updates => {
-      //         campaign.updates = updates;
-      //         return campaign;
-      //       })
-      //       .then(campaign => {
-      //         return findCampaignComments(comment.camp_id)
-      //           .then(comments => {
-      //             campaign.comments = comments;
-      //             return campaign;
-      //           })
-      //           .then(campaign => {
-      //             return campaign;
-      //           });
-      //       });
-      //   });
     );
-}
-// Possible circular dependency issue prevented me from simply calling findById from campaignModel.js in the above function
-// Commented out for now, while I investigate issues on the dev server
-
-function update(id, changes) {
-  return db('comments')
-    .where({ id })
-    .update(changes)
-    .then(() => findById(id));
+  return comments.map((c) => ({ ...c, name: c.org_name || c.sup_name || 'User' }));
 }
 
-function remove(id) {
-  return db('comments')
+async function insert(comment) {
+  await db('comments')
+    .insert(comment);
+  return findCampaignComments(comment.campaign_id);
+}
+
+async function update(id, changes) {
+  await db('comments')
     .where({ id })
-    .del()
-    .then(() => id);
+    .update(changes);
+  return findById(id);
+}
+
+async function remove(id) {
+  await db('comments')
+    .where({ id })
+    .del();
+  return id;
 }
 
 module.exports = {
