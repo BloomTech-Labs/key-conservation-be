53ef293e4569b8171519384190b26769eb21b3c5
David Cruz <david@typokign.com>
2020-05-03 15:31:04 -0700

    Convert database skills enum to table
    Converted the skills enum in Postgres to a table of all possible skills,
    updating the skills table to use a foreign key constraint on the skill
    column, instead of a native enum or check statement.
    
    This will make modifying the set of skills a lot simpler - skills can
    simply be inserted/deleted/updated with INSERT/DELETE/UPDATE SQL
    statements on the skill_enum table.
    
    Also updated knex, pg, and modified docker-compose.yml to include
    default settings for environment variables.

---
+++ app/database/migrations/20200502140659_skill_enum.js
@@ -0,0 +1,65 @@
+const skills = [
+  'ACCOUNTING',
+  'ARCHITECTURE',
+  'AUTO',
+  'APP_DEVELOPMENT',
+  'ART',
+  'AVIATION',
+  'BOATING',
+  'BUSINESS_DEVELOPMENT',
+  'CAMERA_TRAPS',
+  'COMMUNICATION',
+  'COMMUNITY_LIAISON',
+  'CONSTRUCTION',
+  'CRAFT',
+  'CULINARY',
+  'DATA_ANALYSIS',
+  'DATABASE_MANAGEMENT',
+  'DIVING',
+  'DRONE',
+  'ELECTRICITY',
+  'ENGINEERING',
+  'ENTREPRENEURSHIP',
+  'FINANCE',
+  'FUNDRAISING',
+  'GAMING',
+  'GRAPHIC_DESIGN',
+  'HOSPITALITY',
+  'HUMAN_RESOURCES',
+  'INFORMATION_TECHNOLOGY',
+  'LANDSCAPE',
+  'LEGAL',
+  'MANAGEMENT',
+  'MARKETING',
+  'MEDICAL',
+  'MUSICAL',
+  'PHOTOGRAPHY',
+  'PLUMBING',
+  'PUBLIC_RELATIONS',
+  'RENEWABLE_ENERGY',
+  'RESEARCH',
+  'SENSORS',
+  'SOCIAL_MEDIA',
+  'STRATEGY_CONSULTING',
+  'TAXI',
+  'TRANSLATION',
+  'VETERINARY_SERVICES',
+  'VIDEOGRAPHY',
+  'WEB_DESIGN',
+  'WEB_DEVELOPMENT',
+  'WRITING',
+];
+
+exports.up = function (knex) {
+  return knex.schema.createTable('skill_enum', (tbl) => {
+    tbl.string('skill').primary();
+  })
+    .then(() => knex('skill_enum').insert(skills.map((skill) => ({ skill }))))
+    .then(() => knex.schema.alterTable('skills', (tbl) => {
+      tbl.string('skill').notNullable().references('skill_enum.skill').alter();
+    }));
+};
+
+exports.down = function () {
+
+};
