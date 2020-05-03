2e445aa2b0a6f2436e7d23312d34c872b17c292e
Sumeet Bansal <sumeetbansal@gmail.com>
2020-05-02 23:57:53 -0700

    update_of -> is_update

--- app/database/migrations/20300000000000_campaigns_and_posts.js
+++ app/database/migrations/20300000000000_campaigns_and_posts.js
@@ -7,11 +7,11 @@ exports.up = (knex) => knex.schema
       .onDelete('CASCADE');
     table.text('image').notNullable();
     table.text('description').notNullable();
-    table.integer('update_of');
+    table.boolean('is_update');
     table.timestamp('created_at').defaultTo(knex.fn.now());
   })
-  .then(() => knex.schema.raw('INSERT INTO campaign_posts (campaign_id, image, description, created_at) SELECT id, image, description, created_at FROM campaigns ORDER BY id'))
-  .then(() => knex.schema.raw('INSERT INTO campaign_posts (campaign_id, image, description, update_of, created_at) SELECT campaign_id, image, description, campaign_id, created_at FROM campaign_updates'))
+  .then(() => knex.schema.raw('INSERT INTO campaign_posts (campaign_id, image, description, is_update, created_at) SELECT id, image, description, FALSE, created_at FROM campaigns ORDER BY id'))
+  .then(() => knex.schema.raw('INSERT INTO campaign_posts (campaign_id, image, description, is_update, created_at) SELECT campaign_id, image, description, TRUE, created_at FROM campaign_updates'))
   .then(() => Promise.all([
     knex.schema.dropTable('campaign_updates'),
     knex.schema.alterTable('campaigns', (table) => {
@@ -41,6 +41,6 @@ exports.down = (knex) => knex.schema
     table.text('camp_name').notNullable();
   })
   .then(() => Promise.all([
-    knex.schema.raw('UPDATE campaigns SET image = campaign_posts.image, description = campaign_posts.description FROM campaigns AS c JOIN campaign_posts ON c.id = campaign_posts.campaign_id WHERE campaign_posts.update_of IS NULL AND campaigns.id = campaign_posts.campaign_id'),
-    knex.schema.raw('INSERT INTO campaign_updates (campaign_id, user_id, image, description, created_at, camp_name) SELECT campaign_id, user_id, campaign_posts.image, campaign_posts.description, campaign_posts.created_at, campaigns.name as camp_name FROM campaign_posts JOIN campaigns ON campaigns.id = campaign_posts.campaign_id WHERE campaign_posts.update_of IS NOT NULL'),
+    knex.schema.raw('UPDATE campaigns SET image = campaign_posts.image, description = campaign_posts.description FROM campaigns AS c JOIN campaign_posts ON c.id = campaign_posts.campaign_id WHERE campaign_posts.is_update = FALSE AND campaigns.id = campaign_posts.campaign_id'),
+    knex.schema.raw('INSERT INTO campaign_updates (campaign_id, user_id, image, description, created_at, camp_name) SELECT campaign_id, user_id, campaign_posts.image, campaign_posts.description, campaign_posts.created_at, campaigns.name as camp_name FROM campaign_posts JOIN campaigns ON campaigns.id = campaign_posts.campaign_id WHERE campaign_posts.is_update = TRUE'),
   ])).then(() => knex.schema.dropTable('campaign_posts'));
