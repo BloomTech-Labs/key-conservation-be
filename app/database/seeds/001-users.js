72a1992744bf99f3cc007db268e55b8abbbb3877
Sumeet Bansal <sumeetbansal@gmail.com>
2020-05-03 20:46:19 -0700

    fixed seed IDs

--- app/database/seeds/001-users.js
+++ app/database/seeds/001-users.js
@@ -3,35 +3,35 @@ const generateRandomSub = require('../../../util/generateRandomSub');
 const TABLE_NAME = 'users';
 exports.seed = (knex, Promise) => knex(TABLE_NAME).del().then(() => knex(TABLE_NAME).insert([
   {
-    id: 1,
+    // id: 1,
     sub: generateRandomSub(),
     email: 'info@beavertown.org',
     location: 'Pittsburg, PA',
     roles: 'conservationist',
     accepting_help_requests: true,
   }, {
-    id: 2,
+    // id: 2,
     sub: generateRandomSub(),
     email: 'pandalover234@hotmail.com',
     location: 'Phoenix, AZ',
     roles: 'conservationist',
     accepting_help_requests: false,
   }, {
-    id: 3,
+    // id: 3,
     sub: generateRandomSub(),
     email: 'giraffe2tall@gmail.com',
     location: 'Paris, France',
     roles: 'conservationist',
     accepting_help_requests: true,
   }, {
-    id: 4,
+    // id: 4,
     sub: generateRandomSub(),
     email: 'supporter23@conservation.com',
     location: 'Chicago, IL',
     roles: 'supporter',
     accepting_help_requests: true,
   }, {
-    id: 5,
+    // id: 5,
     sub: generateRandomSub(),
     email: 'supporter30@conservation.com',
     location: 'Oakland, CA',
--- app/database/seeds/002-conservationists.js
+++ app/database/seeds/002-conservationists.js
@@ -1,7 +1,7 @@
 const TABLE_NAME = 'conservationists';
 exports.seed = (knex, Promise) => knex(TABLE_NAME).del().then(() => knex(TABLE_NAME).insert([
   {
-    id: 1,
+    // id: 1,
     user_id: 1,
     name: 'Beaver Town',
     link_url: 'google.com',
@@ -11,7 +11,7 @@ exports.seed = (knex, Promise) => knex(TABLE_NAME).del().then(() => knex(TABLE_N
     support_us: null,
   },
   {
-    id: 2,
+    // id: 2,
     user_id: 2,
     name: 'Panda Lovers',
     link_url: 'google.com',
@@ -21,7 +21,7 @@ exports.seed = (knex, Promise) => knex(TABLE_NAME).del().then(() => knex(TABLE_N
     support_us: null,
   },
   {
-    id: 3,
+    // id: 3,
     user_id: 3,
     name: 'Giraffe 2 Tall',
     link_url: 'google.com',
