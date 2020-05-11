035f8a3bea6704b97178cb9945604fd2a7f9649f
Mohammad Tourjoman <mtourjoman01@hotmail.com>
2020-05-11 03:20:45 -0700

    Trying again

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const https = require('https');
+const http = require('http');
 const app = require('./app');
 const { WebSocketManager } = require('./app/websockets');
 const log = require('./app/logger');
 
 const port = process.env.PORT || 8000;
 
-const server = https.createServer(app);
-
-app.listen(port, () => {
-  log.info(`listening on port ${port}`);
-});
+const server = http.createServer(app);
 
 WebSocketManager.start(server);
+
+server.listen(port, () => {
+  log.info(`listening on port ${port}`);
+});
