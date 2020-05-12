977dfeb93f79a9cc652d83aab076e7bcf7d73afb
David Cruz <david@typokign.com>
2020-05-12 14:59:34 -0700

    Restore campaigns API and model (#230)

--- app/database/models/campaignModel.js
+++ app/database/models/campaignModel.js
@@ -29,6 +29,53 @@ async function findById(id) {
   return campaign;
 }
 
+async function findAll(filters) {
+  const { skill } = filters;
+  try {
+    let campaigns = db('campaigns')
+      .join('users', 'users.id', 'campaigns.user_id')
+      .leftJoin('conservationists as cons', 'cons.user_id', 'users.id')
+      .select(
+        'users.profile_image',
+        'users.location',
+        'campaigns.*',
+        'campaigns.id as campaign_id',
+        'campaigns.name as camp_name',
+        'cons.name as org_name',
+      )
+      .where({ 'users.is_deactivated': false });
+
+    if (skill) {
+      campaigns = campaigns
+        .join(
+          'skilled_impact_requests',
+          'skilled_impact_requests.campaign_id',
+          'campaigns.id',
+        )
+        .select(
+          'skilled_impact_requests.skill',
+          'skilled_impact_requests.id as skilled_impact_request_id',
+        )
+        .where('skilled_impact_requests.skill', skill);
+    }
+
+    campaigns = await campaigns;
+
+    return Promise.all(campaigns.map(async (c) => {
+      const { image, description } = await CampaignPosts.findOriginalCampaignPostByCampaignId(c.id);
+      const comments = await Comments.findCampaignComments(c.id);
+      return {
+        ...c,
+        image,
+        description,
+        comments,
+      };
+    }));
+  } catch (err) {
+    throw new Error(err.message);
+  }
+}
+
 async function insert(campaign) {
   log.verbose(`Inserting new campaign ${campaign}`);
   try {
@@ -57,5 +104,5 @@ async function remove(id) {
 }
 
 module.exports = {
-  findById, insert, remove, update,
+  findById, findAll, insert, remove, update,
 };
