478f3f61d4991bd240b246c86cc7f6aaf8d7cce7
Mohammad Tourjoman <mtourjoman01@hotmail.com>
2020-05-16 12:36:39 -0700

    Overhaul feed (#235)
    * Initial WebSockets Hello World
    
    * Implement feed endpoint
    
    * Essential feed components implemented
    
    * Fix incorrect join usage
    
    * Nuke Dropped
    
    * Implement web sockets
    
    * Comment out debug code
    
    * Trying something
    
    * Trying something else...
    
    * Attemping to get WebSockets to work from heroku
    
    * Trying again
    
    * Trying again
    
    * Implement filter by date for feed
    
    * Send message on update post
    
    * Fixes implemented
    
    * Fix response after posting a campaign
    
    * Expand query parameters for feed
    
    * Modify feed to be date based instead of index-based

--- app/api/feed/index.js
+++ app/api/feed/index.js
@@ -1,32 +1,18 @@
 const express = require('express');
 const CampaignPosts = require('../../database/models/campaignPostsModel');
-const SkillsEnum = require('../../database/models/skillsEnum');
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
   // size: Size of data to send back
-  // startAt: Which post to start sending data from,
-  // 0 being newest post
+  // startAt: Date of post to start sending data from,
+  // date to filter by
+  // dateOrder: 0 to get newer, 1 to get older.
   const { size, startAt, date } = req.query;
 
-  let { skill } = req.query;
-
-  // verify skill if passed in
-  if (skill) {
-    skill = skill.toUpperCase().replace(' ', '_');
-    if (!Object.keys(SkillsEnum).includes(skill)) {
-      res.status(400).json({ message: 'Invalid skill entered' });
-    }
-  }
-
   try {
-    const feed = await CampaignPosts.getMostRecentPosts(
-      startAt,
-      size,
-      date,
-      skill,
-    );
+    const feed = await CampaignPosts.getMostRecentPosts(startAt, size, date);
+
     return res.status(200).json(feed);
   } catch (err) {
     return res.status(500).json({
