fcc36e63cd087db85ab2cc5164a4801f1654285e
shravanhariharan2 <smhariha@ucsd.edu>
2020-05-22 13:40:23 -0700

    add skilled impact requests and project goals response to GET /feed

--- app/database/models/campaignPostsModel.js
+++ app/database/models/campaignPostsModel.js
@@ -12,6 +12,8 @@ async function getMostRecentPosts(startAt = undefined, size = 8, date) {
   let posts = db('campaign_posts')
     .join('campaigns', 'campaign_posts.campaign_id', 'campaigns.id')
     .join('users', 'campaigns.user_id', 'users.id')
+    .leftJoin('skilled_impact_requests', 'campaigns.id', 'skilled_impact_requests.campaign_id')
+    .leftJoin('project_goals', 'skilled_impact_requests.id', 'project_goals.skilled_impact_request_id')
     .leftJoin('conservationists', 'users.id', 'conservationists.user_id')
     .leftJoin('comments', 'comments.campaign_id', 'campaign_posts.campaign_id')
     .whereNot('users.is_deactivated', true)
@@ -26,9 +28,14 @@ async function getMostRecentPosts(startAt = undefined, size = 8, date) {
       'users.location',
       'users.profile_image',
       'conservationists.name as org_name',
+      // use distinct to eliminate duplicate rows from left joins
       db.raw(
         // eslint-disable-next-line quotes
-        `ARRAY_AGG(json_build_object('id', comments.id, 'user_id', comments.user_id, 'created_at', comments.created_at, 'body', comments.body)) filter (where comments.id is not null) as comments`,
+        `ARRAY_AGG(DISTINCT jsonb_build_object('id', skilled_impact_requests.id, 'skill', skilled_impact_requests.skill, 'project_goals', (select json_agg(project_goals) FROM (SELECT * FROM project_goals WHERE skilled_impact_request_id = skilled_impact_requests.id) AS project_goals))) FILTER (WHERE skilled_impact_requests.id IS NOT null) AS skilled_impact_requests`,
+      ),
+      db.raw(
+        // eslint-disable-next-line quotes
+        `ARRAY_AGG(DISTINCT jsonb_build_object('id', comments.id, 'user_id', comments.user_id, 'created_at', comments.created_at, 'body', comments.body)) filter (where comments.id is not null) as comments`,
       ),
     )
     .groupBy(
@@ -40,6 +47,7 @@ async function getMostRecentPosts(startAt = undefined, size = 8, date) {
       'users.location',
       'users.profile_image',
       'conservationists.name',
+      'skilled_impact_requests.id',
     )
     .limit(72);
 
