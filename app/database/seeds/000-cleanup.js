e2588a75a81ab4c19fde0ffa927f1e76cd199b84
David Cruz <david@typokign.com>
2020-05-31 14:28:32 -0700

    Fix database seeds

--- app/database/seeds/000-cleanup.js
+++ app/database/seeds/000-cleanup.js
@@ -1,5 +1,5 @@
 const cleaner = require('knex-cleaner');
 
 exports.seed = (knex) => cleaner.clean(knex, {
-  ignoreTables: ['knex_migrations', 'knex_migrations_lock'],
+  ignoreTables: ['knex_migrations', 'knex_migrations_lock', 'skill_enum'],
 });
