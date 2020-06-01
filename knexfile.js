cf97f1cf624ebd5953c8187f5cf70bb04ac7c578
Rasha Moumneh <rasha@fastmail.com>
2020-06-01 19:14:00 -0400

    Setup test environment

--- knexfile.js
+++ knexfile.js
@@ -11,10 +11,35 @@ const dbConnection = process.env.DATABASE_URL || {
   database: process.env.DATABASE_NAME,
 };
 
+const dbTestingConnection = process.env.DATABASE_URL || {
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASS,
+  database: process.env.DATABASE_TEST_NAME,
+};
+
 module.exports = {
   development: {
     client: 'pg',
     connection: dbConnection,
+    migrations: {
+      directory: './app/database/migrations',
+      tablename: 'knex_migrations',
+    },
+    seeds: {
+      directory: './app/database/seeds',
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    useNullAsDefault: true,
+  },
+
+  testing: {
+    client: 'pg',
+    connection: dbTestingConnection,
     migrations: {
       directory: './app/database/migrations',
       tablename: 'knex_migrations',
