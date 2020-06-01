4bc7e0080b01fa5b2070e4af607f9a097a710cb3
Rasha Moumneh <rasha@fastmail.com>
2020-06-01 19:52:23 -0400

    Add test file for vetting endpoint

--- app/api/vetting/index.spec.js
+++ app/api/vetting/index.spec.js
@@ -1,1 +1,9 @@
 const server = require('../../../app');
+const request = require('supertest');
+
+describe('vetting', () => {
+  it('get /', async () => {
+    const res = await request(server).get('/vetting');
+    expect(res.status).toBe(200);
+  });
+});
